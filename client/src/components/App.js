94770010e93416c745eac4f4ddb9f67af8814860
Jared <jbeckersalax9@aol.com>
2023-01-03 21:42:37 -0500

    unload function removed from logins useEffect

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchUser, logOutUser } from "../reducers/userSlice";
+import { fetchUser } from "../reducers/userSlice";
 import "../styles/App.css"
 import Login from "./Login";
 import NavRoutes from "./NavRoutes";
@@ -19,7 +19,6 @@ function App() {
   } else {
     return <NavRoutes />
   }
-    
 }
 
 export default App;
