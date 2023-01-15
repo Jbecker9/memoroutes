bbfacffce38fb4822ecc9399cf65b106375cf3f3
Jared <jbeckersalax9@aol.com>
2023-01-15 00:26:30 -0500

    bin issue

--- bin/memoroutes-build.sh
+++ bin/memoroutes-build.sh
@@ -4,9 +4,8 @@ set -o errexit
 
 # Add build commands for front end
 rm -rf public
-rvm use 3.1.2
 npm install --prefix client && npm run build --prefix client
 cp -a client/build/. public/
 
 bundle install
-bundle exec rails db:migrate 
+bundle exec rake db:migrate 
